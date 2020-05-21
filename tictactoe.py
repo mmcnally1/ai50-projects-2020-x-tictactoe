15943cdcd404e9e5718b35e4e6501fd3347c44ac
mmcnally1 <62566228+mmcnally1@users.noreply.github.com>
2020-05-21 15:54:34 -0500

    finished actions, maybe finished terminal, utility

--- tictactoe.py
+++ tictactoe.py
@@ -17,29 +17,44 @@ def initial_state():
             [EMPTY, EMPTY, EMPTY],
             [EMPTY, EMPTY, EMPTY]]
 
-
-def player(board):
-    """
-    Returns player who has the next turn on a board.
-    """
+def open_cells(board):
     open_cells = 0
     for i in range(len(board)):
         for j in range(len(board)):
             if board[i][j] == None:
                 open_cells += 1
-    
-    if open_cells == 0:
+    return open_cells
+
+def player(board):
+    """
+    Returns player who has the next turn on a board.
+    """
+    if open_cells(board) == 0:
         return "terminal"
-    elif open_cells % 2 == 0:
+    elif open_cells(board) % 2 == 0:
         return O
     else:
         return X
-
+    
 def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    raise NotImplementedError
+    moves = set()
+    for i in range(len(board)):
+        for j in range(len(board)):
+            if board[i][j] == None:
+                square = (i,j)
+                moves.add(square)
+    if len(moves) > 0:
+        return moves
+    else:
+        return "terminal"
+
+board = [[X, X, X],
+        [X, X, X],
+        [X, X, X]]
+print(actions(board))
 
 
 def result(board, action):
@@ -60,14 +75,22 @@ def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    raise NotImplementedError
+    if winner(board) == X or winner(board) == O or open_cells(board) == 0:
+        return True
+    else:
+        return False
 
 
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
+    if winner(board) == X:
+        return 1
+    elif winner(board) == O:
+        return -1
+    else:
+        return 0
 
 
 def minimax(board):
